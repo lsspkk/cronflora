e2f3b1fd192c2bd61082952a45432860dd8ad484
lsspkk <lsspkk@gmail.com>
2026-01-19 11:47:14 +0200

    feat: update MenuBar to use vite.svg logo instead of text

--- swa-site/src/components/MenuBar.tsx
+++ swa-site/src/components/MenuBar.tsx
@@ -15,7 +15,7 @@ export function MenuBar({ documentPath, isLoaded, onLoad, onSave, onSearchReplac
   return (
     <div className='bg-gray-800 text-white px-4 py-2 flex items-center justify-between'>
       <div className='flex items-center gap-4'>
-        <span className='font-semibold text-lg'>CronFlora</span>
+        <img src="/vite.svg" alt="CronFlora" className="w-6 h-6" />
 
         <div className='flex items-center gap-2'>
           <button
